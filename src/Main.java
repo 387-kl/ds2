dff879c5f47463644cf6aceb389f672b4aef5814
artur <artur_lychik@mail.ru>
2025-03-05 18:12:08 +0500

    ihit

---
+++ src/Main.java
@@ -0,0 +1,120 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a = 4;
+        byte b = 5;
+        short c = 2;
+        long g = 12;
+        float r = 31;
+        double h =14;
+        double p = a+b+c+g+r+h;
+        System.out.println("вобщее количесво цифр " + p + " От меньших к старшим");
+
+
+        double q = 27.12;
+        double w = 987678965549f;
+        double e = 2.768;
+        int t = -159;
+        int y = 27897;
+        byte u = 67;
+        int l = 568;
+        double i = q+w+e+t+t+y+u+l;
+        System.out.println("Ответ " + i);
+
+
+        int luds = 23;
+        int anna = 27;
+        int ekat = 30;
+        int boom = luds+anna+ekat;
+        System.out.println("Всего учеников " + boom);
+        int list = 480;
+        System.out.println("Всего купили листов " + list);
+        int fiss = list / boom;
+        System.out.println("По" + fiss +"л Получат ученики");
+
+        int byet = 16;
+        int byit =2;
+        int byt = byet / byit;
+        System.out.println("Бутылок за 1 мин " +byt + " Штук");
+        int mon =20;
+        int min = byt * mon;
+        System.out.println("За 20 минут мы получим " + min +" Бутылок");
+        int syt = 1440;
+        int sot = syt * byt;
+        System.out.println("В сутки мы получим " + sot + " Бутылок");
+        int qw = 4320;
+        int syto = qw * byt;
+        System.out.println("За трое суток мы получим " + syto + " Бутылок");
+        int az = 43200;
+        int pl = az * byt;
+        System.out.println("За месяц мы получим " + pl + " Буытлок");
+
+
+        int sk = 120;
+        int bk = 6;
+        int kl = sk / bk;
+        System.out.println("Всего " + kl + " Классов");
+        int bkk = 4;
+        int bp = kl * bkk;
+        System.out.println("Коричневой красски " + bp);
+        int bkb = sk - bp;
+        System.out.println("Белой красски " + bkb);
+        System.out.println("В школе, где " + kl + " классов,нужно " + bkb + " банок белой краски и " + bp + " банок коричневой краски");
+
+
+        int ba = 5;
+        int bg = 80;
+        int ban = ba * bg;
+        System.out.println("Всего грамм бонана " + ban);
+        int mo = 200;
+        int mg = 105;
+        int mol = mo * mg;
+        System.out.println("Всего грамм в молоке "+ mol);
+        int mp = 2;
+        int br = 100;
+        int mrp = mp * br;
+        System.out.println("Всего грам мороженого " +mrp);
+        int ia = 4;
+        int ig = 70;
+        int ii = ia * ig;
+        System.out.println("Всего грамм яиц "+ ii);
+        int vsp = ban + mol + mrp + ii;
+        System.out.println("Всего грамм в приему пищи спорстсмена " + vsp);
+        int kg = 1000;
+        int kd = vsp / kg;
+        System.out.println("Всего в приеме пищи " + kd + " килограмм");
+
+
+        int xp = 7 * 1000;
+        System.out.println("Вес в граммах " + xp);
+        int yg = 250;
+        int gg = xp / yg;
+        System.out.println("Потребуеться " + gg + " Дней если по 250 грамм");
+        int ng = 500;
+        int fg = xp / ng;
+        System.out.println("Потребуеться " + fg + " Дней если по 500 грамм");
+        int xi = gg + fg;
+        int xh = xi / 2;
+        System.out.println("Cреднее время похудения " + xh + " Дней");
+
+
+        int ma = 67760 * 110 / 100;
+        System.out.println("10 процентов у кристины " + ma);
+        int ms = 74536 - 67760;
+        System.out.println("Разница " + ms);
+        int de = 83690 * 110/ 100;
+        System.out.println("10 процентов у дениса " + de);
+        int ds = 92059 - 83690;
+        System.out.println("Разница " + ds);
+        int kr = 76230 * 110 / 100;
+        System.out.println("10 процентов у кристины " + kr);
+        int ks = 83853 - 76230;
+        System.out.println("Разница у кристины " + ks);
+        System.out.println("Маша теперь получает " + ma + " рублей. Годовой доход вырос на " + ms + " рублей");
+        System.out.println("Денис теперь получает " + de + " рублей. Годовой доход вырос на " + ds + " рублей"
+        );
+        System.out.println("Кристина тепрь получает " + kr + " рублей. годовой доход вырос на " + ks + " рублей");
+
+
+    }
+}
