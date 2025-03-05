3d937fb0b960235a477bba7acc955cde71b97583
artur <artur_lychik@mail.ru>
2025-03-05 20:58:40 +0500

    dimascha comimit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         int a = 4;
+        System.out.println("Перевная инт" + a);
         byte b = 5;
         short c = 2;
         long g = 12;
